512f1eae35cdbc6d5e5d206f853f1115cfe22262
Gabriel Lopez <gabo_71096@hotmail.com>
2024-03-08 11:45:02 +1300

    Chore: Configure CORS

--- server/Program.cs
+++ server/Program.cs
@@ -5,8 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSignalR();
 builder.Services.AddCors((options) =>
+{
     options.AddDefaultPolicy((policyOpts) =>
-        policyOpts.WithOrigins("http://localhost:5173")));
+    {
+        policyOpts
+            .WithOrigins("http://localhost:5173")
+            .WithHeaders(["x-requested-with", "x-signalr-user-agent"])
+            .AllowCredentials();
+    });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,11 +26,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors();
 }
 
 app.UseHttpsRedirection();
 
 app.MapHub<ChatHub>("/hub");
 
-app.UseCors();
 app.Run();
