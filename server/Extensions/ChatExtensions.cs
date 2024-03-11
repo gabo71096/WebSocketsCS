79a1ae6c5a9084540e2c45eeabb228fa7bbf1b34
Gabriel Lopez <gabo_71096@hotmail.com>
2024-03-12 10:43:42 +1300

    Chore: Remove extension method for chats

--- server/Extensions/ChatExtensions.cs
+++ server/Extensions/ChatExtensions.cs
@@ -5,11 +5,6 @@ namespace WebSocketsCS.Extensions
 {
     public static class ChatExtensions
     {
-        public static List<ChatDTO> MapToDTO(this Chat[] chats)
-        {
-            return chats.Select((chat) => chat.MapToDTO()).ToList();
-        }
-
         public static ChatDTO MapToDTO(this Chat chat)
         {
             return new ChatDTO
