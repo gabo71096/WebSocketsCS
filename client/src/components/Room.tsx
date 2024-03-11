d1951ce28b753cef86cb2122a91aecbae9eac4fc
Gabriel Lopez <gabo_71096@hotmail.com>
2024-03-12 10:43:15 +1300

    Chore: FowardRef and getMessages on mount

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,14 +1,24 @@
-import { Paper, Typography } from "@mui/material";
-import { useEffect } from "react";
-import { addToGroup, removeFromGroup } from "../utilities/socket";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { forwardRef, useEffect } from "react";
+import { addToGroup, connection, removeFromGroup } from "../utilities/socket";
+import { ChatMessage } from "../types";
+import { useLoadingStore } from "../utilities/store";
 
 interface Props {
+  messages: ChatMessage[];
   selectedRoom: string;
 }
 
-export default function Room({ selectedRoom }: Props) {
+export const Room = forwardRef(({ messages, selectedRoom }: Props, ref) => {
+  const { loading, setLoading } = useLoadingStore();
+
   useEffect(() => {
+    setLoading(true);
+
     addToGroup(selectedRoom);
+
+    connection.invoke("GetMessagesForRoom", selectedRoom).then(() => setLoading(false));
 
     return () => {
       removeFromGroup(selectedRoom);
@@ -16,8 +26,18 @@ export default function Room({ selectedRoom }: Props) {
   }, [selectedRoom]);
 
   return (
-    <Paper className="h-96 mb-4 p-4" variant="outlined">
-      <Typography variant="body1">{selectedRoom}</Typography>
-    </Paper>
+    <Box className="border h-96 mb-4 overflow-auto p-4 rounded" ref={ref}>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        messages?.map((message, index) => (
+          <div key={index}>
+            <Typography variant="body1">
+              {message.username}: {message.message}
+            </Typography>
+          </div>
+        ))
+      )}
+    </Box>
   );
-}
+});
