e2309be2a84873b2daa32cd0df90d2e0a7fc098a
Gabriel Lopez <gabo_71096@hotmail.com>
2024-03-15 16:16:14 +1300

    Chore: Change expected argument type

--- client/src/utilities/utilities.ts
+++ client/src/utilities/utilities.ts
@@ -1,3 +1,3 @@
-export function scrollToBottom(element?: HTMLDivElement) {
+export function scrollToBottom(element: HTMLDivElement | null) {
   element?.scrollTo({ behavior: "smooth", top: element?.scrollHeight });
 }
